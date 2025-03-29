258a54154c7e39379a15ac9093ffc9a4dfc3fd5b
MultiX0 <ishiiro1990@gmail.com>
2025-03-29 04:07:59 +0300

    add friends_leaderboard

--- lib/features/ranking/pages/leaderboard_page.dart
+++ lib/features/ranking/pages/leaderboard_page.dart
@@ -12,6 +12,7 @@ class LeaderboardPage extends ConsumerStatefulWidget {
 
 class _LeaderboardPageState extends ConsumerState<LeaderboardPage> {
   int selectedIndex = 0;
+  bool btnLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,80 +27,147 @@ class _LeaderboardPageState extends ConsumerState<LeaderboardPage> {
             children: [
               const SizedBox(height: 15),
               buildCategoryChange(),
-              const SizedBox(height: 15),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: AppColors.primary),
-                  color: AppColors.primary.withValues(alpha: .2),
+              if (selectedIndex == 0) ...[
+                const SizedBox(height: 15),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: AppColors.primary),
+                    color: AppColors.primary.withValues(alpha: .2),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(AppLocalizations.of(context).your_rank),
+                      const SizedBox(height: 10),
+                      buildCard(userRank, AppColors.primary, user!, true),
+                    ],
+                  ),
                 ),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(AppLocalizations.of(context).your_rank),
-                    const SizedBox(height: 10),
-                    buildCard(userRank, AppColors.primary, user!, true),
-                  ],
-                ),
-              ),
+              ],
               const SizedBox(height: 15),
               Expanded(
-                child: ref
-                    .watch(getAllRankingLeaderboardProvider)
-                    .when(
-                      data: (players) {
-                        return RefreshIndicator(
-                          color: AppColors.whiteColor,
-                          backgroundColor: AppColors.primary.withValues(alpha: 0.5),
-                          onRefresh: () async {
-                            await Future.delayed(const Duration(milliseconds: 300), () {
-                              ref.invalidate(getAllRankingLeaderboardProvider);
-                            });
+                child: IndexedStack(
+                  index: selectedIndex,
+                  children: [
+                    ref
+                        .watch(getAllRankingLeaderboardProvider)
+                        .when(
+                          data: (players) {
+                            return leaderboardBody(players, user!);
                           },
-                          child: ListView.builder(
-                            itemCount: players.length,
-                            itemBuilder: (context, i) {
-                              Color playerColor;
-                              switch (i + 1) {
-                                case 1:
-                                  playerColor = AppColors.primary;
-                                case 2:
-                                  playerColor = Colors.green[300]!;
-                                case 3:
-                                  playerColor = Colors.blueGrey[200]!;
-
-                                default:
-                                  playerColor = AppColors.whiteColor;
-                              }
-                              final player = players[i];
-                              if (player.id == user.id) {
-                                return const SizedBox();
-                              }
-                              return Padding(
-                                padding: const EdgeInsets.fromLTRB(5, 15, 5, 5),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    buildCard(i, playerColor, player, false),
-                                    const SizedBox(height: 15),
-                                    Divider(
-                                      color: AppColors.descriptionColor.withValues(alpha: .15),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                          ),
-                        );
-                      },
-                      error: (error, _) => Center(child: Text(error.toString())),
-                      loading: () => BeatLoader(),
-                    ),
+                          error: (error, _) => Center(child: Text(error.toString())),
+                          loading: () => BeatLoader(),
+                        ),
+                    ref
+                        .watch(getFriendsLeaderboard)
+                        .when(
+                          data: (players) {
+                            if (players.isEmpty) {
+                              return friendsEmptyState();
+                            }
+                            return leaderboardBody(players, user!);
+                          },
+                          error: (error, _) => Center(child: Text(error.toString())),
+                          loading: () => BeatLoader(),
+                        ),
+                  ],
+                ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget friendsEmptyState() => Center(
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: SizedBox(
+        child: SystemCard(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(LucideIcons.shield_alert, color: AppColors.primary, size: 45),
+              const SizedBox(height: 15),
+              Text(
+                AppLocalizations.of(context).no_friends,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              const SizedBox(height: 15),
+              if (btnLoading) ...[
+                BeatLoader(),
+              ] else ...[
+                MainAppButton(
+                  onTap: () async {
+                    setState(() {
+                      btnLoading = true;
+                    });
+                    await Future.delayed(const Duration(milliseconds: 500), () {
+                      ref.invalidate(getFriendsLeaderboard);
+                    });
+                  },
+                  title: AppLocalizations.of(context).refresh,
+                ),
+                const SizedBox(height: 15),
+                MainAppButton(
+                  onTap: () => context.push(Routes.addFriendsPage),
+                  title: AppLocalizations.of(context).add_friends_btn,
+                ),
+              ],
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+
+  RefreshIndicator leaderboardBody(List<UserModel> players, UserModel me) {
+    return RefreshIndicator(
+      color: AppColors.whiteColor,
+      backgroundColor: AppColors.primary.withValues(alpha: 0.5),
+      onRefresh: () async {
+        await Future.delayed(const Duration(milliseconds: 300), () {
+          if (selectedIndex == 0) {
+            ref.invalidate(getAllRankingLeaderboardProvider);
+          } else {
+            ref.invalidate(getFriendsLeaderboard);
+          }
+        });
+      },
+      child: ListView.builder(
+        itemCount: players.length,
+        itemBuilder: (context, i) {
+          Color playerColor;
+          switch (i + 1) {
+            case 1:
+              playerColor = AppColors.primary;
+            case 2:
+              playerColor = Colors.green[300]!;
+            case 3:
+              playerColor = Colors.blueGrey[200]!;
+
+            default:
+              playerColor = AppColors.whiteColor;
+          }
+          final player = players[i];
+          if (player.id == me.id && selectedIndex != 1) {
+            return const SizedBox();
+          }
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(5, 15, 5, 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildCard(i, playerColor, player, false),
+                const SizedBox(height: 15),
+                Divider(color: AppColors.descriptionColor.withValues(alpha: .15)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
@@ -181,14 +249,15 @@ class _LeaderboardPageState extends ConsumerState<LeaderboardPage> {
     required int index,
     required String text,
   }) {
-    bool isArabic = ref.watch(localeProvider).languageCode == 'ar';
+    // bool isArabic = ref.watch(localeProvider).languageCode == 'ar';
 
     bool isActive = selectedIndex == index;
     return GestureDetector(
       onTap: () {
         ref.read(soundEffectsServiceProvider).playSystemButtonClick();
-        if (index == 0 && isActive) return;
-        CustomToast.soon(isArabic);
+        setState(() {
+          selectedIndex = index;
+        });
       },
       child: AnimatedContainer(
         duration: duration,
